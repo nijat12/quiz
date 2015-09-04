bf0bfe4f818e6d01bbcc1363bbe2828aa92db71f
Nijat Zeynalov <nijat12@gmail.com>
2015-09-04 18:03:09 -0400

    reskinned the UI

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,21 +14,42 @@ var app = angular
     'ngAria',
     'ngMessages',
     'ngResource',
-    'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.router',
+    'ngDialog'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+
+  //.run(['$location', function($location){
+  //  $location.path("/login");
+  //}])
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    window.paceOptions = {
+      ajax: {
+        trackMethods:      ['GET', 'POST', 'PUT'],  // takes all verbs...
+        trackWebSockets:  false                     // off because LiveReload will cause pace to load forever...
+      },
+      document:            false,                   // we only care about XHR loads...
+      restartOnPushState:  false                    // we only care about XHR requests...
+    };
+
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('header', {
+        templateUrl: 'views/header.html'
+        //controller: 'headerCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+      .state('header.login', {
+        url:'/',
+        templateUrl: 'views/login.html'
+        //controller: 'headerCtrl'
       })
-      .otherwise({
-        redirectTo: '/'
+      .state('header.createQuiz', {
+        url:'/create',
+        templateUrl: 'views/createQuiz.html'
       });
   });
