b63d911e7a9fdbc2e8aa7ad345a3d4496cf18add
David Wong <david492000@gmail.com>
2015-12-23 16:02:33 -0500

    latest

---
+++ server/medicalquiz/src/main/java/medicalquiz/models/Explanation.java
@@ -0,0 +1,29 @@
+package medicalquiz.models;
+
+/**
+ * Created by david492000 on 12/23/15.
+ */
+public class Explanation {
+    private String label;
+
+    private String image;
+
+    public Explanation() {
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
